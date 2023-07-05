248e78f3b30b371eb760aaef859e05976a14997a
domi <t01048187045@gmail.com>
2023-07-06 01:52:03 +0900

    진짜 편한

--- Assets/TileWay.cs
+++ Assets/TileWay.cs
@@ -12,6 +12,12 @@ public class TileWay
         get => new(x, y);
     }
 
+    // class끼리 더하는거임 아무튼 개편함
+    public static TileWay operator +(TileWay a, TileWay b)
+    {
+        return new TileWay(a.x + b.x, a.y + b.y);
+    }
+
     public TileWay(int _x, int _y)
     {
         x = _x;
