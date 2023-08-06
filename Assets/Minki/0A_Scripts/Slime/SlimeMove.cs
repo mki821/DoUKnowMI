97b13508be04b4badeec5cbb32078287914890df
dazb <jiwoo2426p@gmail.com>
2023-08-07 02:50:07 +0900

    ds

--- Assets/Minki/0A_Scripts/Slime/SlimeMove.cs
+++ Assets/Minki/0A_Scripts/Slime/SlimeMove.cs
@@ -10,13 +10,25 @@ public class SlimeMove : MonoBehaviour
     private LayerMask layer = 7;
     private Animator animator;
 
+#region Slime_Idle
     private const float Slime_Idle_left = 0;
     private const float Slime_Idle_diagonal_left = 0.2f;
     private const float Slime_Idle = 0.4f;
     private const float Slime_Idle_diagonal_right = 0.6f;
     private const float Slime_Idle_right = 0.8f;
     private const float Slime_Idle_back = 1;
+    
+#endregion
 
+#region Slime_Atk
+    private const float Slime_left_Atk = 0;
+    private const float Slime_diagonal_left_Atk = 0.2f;
+    private const float Slime_Atk = 0.4f;
+    private const float Slime_diagonal_right_Atk = 0.6f;
+    private const float Slime_right_Atk = 0.8f;
+    private const float Slime_back_Atk = 1;
+
+#endregion
 
     private void Start() {
         movePos.Add(transform.position);
@@ -32,8 +44,11 @@ public class SlimeMove : MonoBehaviour
         float t = 0;
         for(int i = 1; i < movePos.Count; i++) {
             if (i == movePos.Count - 1) {
-                //CameraManager.SlowCameraEnable(transform.position);
-                StartCoroutine(TimeScale());
+                // 일단 보류
+                // if () {
+                //     CameraManager.SlowCameraEnable(movePos[i]);
+                //     StartCoroutine(TimeScale());
+                // }
             }
             float distance = (movePos[i - 1] - movePos[i]).magnitude;
             SetSlimeDir(i);
@@ -52,6 +67,7 @@ public class SlimeMove : MonoBehaviour
     }
 
     private IEnumerator TimeScale() {
+        //yield return null;
         Time.timeScale = 0.05f;
         print(Time.timeScale);
         yield return new WaitForSecondsRealtime(1f);
@@ -60,14 +76,18 @@ public class SlimeMove : MonoBehaviour
     }
 
     private void SetSlimeDir(int i) {
+        animator.SetTrigger("IsAtk");
         Vector2 dir  = movePos[i] - movePos[i - 1];
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        if (Mathf.Abs(angle - (-90f)) < 0.0001f)
-            animator.SetFloat("Slime_Idle", Slime_Idle);
-        else if (Mathf.Abs(angle - (180f)) < 0.0001f)
-            animator.SetFloat("Slime_Idle", Slime_Idle_left);
-        else if (Mathf.Abs(angle - (0f)) < 0.0001f)
-            animator.SetFloat("Slime_Idle", Slime_Idle_right);
+        if (Mathf.Abs(angle - (-90f)) < 0.0001f) {
+            animator.SetFloat("Slime_Atk", Slime_Atk);
+        }
+        else if (Mathf.Abs(angle - (180f)) < 0.0001f) {
+            animator.SetFloat("Slime_Atk", Slime_left_Atk);
+        }
+        else if (Mathf.Abs(angle - (0f)) < 0.0001f) {
+            animator.SetFloat("Slime_Atk", Slime_right_Atk);
+        }
         // 더 추가 해야됨
         print("각도" + angle);
     }
