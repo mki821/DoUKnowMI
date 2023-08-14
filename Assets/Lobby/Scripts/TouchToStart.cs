dbe72860ac1021c3a7661caa067814b7b576527d
domi <domi@domi.kr>
2023-08-14 21:12:49 +0900

    ㅁㄴㅇㄹ

--- Assets/Lobby/Scripts/TouchToStart.cs
+++ Assets/Lobby/Scripts/TouchToStart.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class TouchToStart : MonoBehaviour
 {
+    [SerializeField] Image _toutchBar;
+
+    private void Start() 
+    {
+        _toutchBar.DOFade(1, .4f).SetLoops((int)LoopType.Yoyo);
+    }
+    
     void Update()
     {
         if (Input.touchCount > 0) {
