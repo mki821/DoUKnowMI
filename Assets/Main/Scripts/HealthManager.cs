2e96047a01b47c5908c29aa87b374f1f247295a4
domi <domi@domi.kr>
2023-08-21 02:47:26 +0900

    체력 시수템

--- Assets/Main/Scripts/HealthManager.cs
+++ Assets/Main/Scripts/HealthManager.cs
@@ -1,16 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using domi.DB;
+using TMPro;
 using UnityEngine;
 
-public class Refill : MonoBehaviour
+public class HealthManager : MonoBehaviour
 {
-    DBstruct _DBstruct;
-    private void Start() {
-        StartCoroutine(Wait10Minute());
+    static HealthManager instance;
+    DBstruct _db;
+
+    [SerializeField] TextMeshProUGUI _healthT;
+
+    private void Awake() {
+        if (instance == null)
+            instance = this;
     }
-    IEnumerator Wait10Minute() {
-        yield return new WaitForSecondsRealtime(600);
-        _DBstruct.health++;
+
+    private void Start() {
+        _db = DBmanager.GetData();
+        UpdateText();
+    }
+
+    public static void Give(int value = 1) {
+        instance._db.health = Mathf.Min(instance._db.health + value, 5);
+        DBmanager.Save();
+        UpdateText();
+    }
+
+    public static bool Try(int value = 1) {
+        if (instance._db.health - value < 0) return false;
+
+        instance._db.health -= value;
+        DBmanager.Save();
+        UpdateText();
+        return true;
+    }
+
+    static void UpdateText() {
+        instance._healthT.text = instance._db.health.ToString();
     }
 }
