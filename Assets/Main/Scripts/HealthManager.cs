2c7f9adaaf6be011cf4e372c4c5f5fafdaa6bece
domi <domi@domi.kr>
2023-08-22 01:31:21 +0900

    체력 ㄷㄷ

--- Assets/Main/Scripts/HealthManager.cs
+++ Assets/Main/Scripts/HealthManager.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using domi.DB;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class HealthManager : MonoBehaviour
 {
-    static long TIME_LEFT = /* 6000000000 */ 1000000000;
+    static long TIME_LEFT = 6000000000;
     static HealthManager instance;
     public static int health {
         get => instance._db.health;
@@ -15,6 +16,8 @@ public class HealthManager : MonoBehaviour
     DBstruct _db;
 
     [SerializeField] TextMeshProUGUI _healthT;
+    [SerializeField] TextMeshProUGUI _timeT;
+    bool activeTimer = false;
 
     private void Awake() {
         if (instance == null)
@@ -32,16 +35,25 @@ public class HealthManager : MonoBehaviour
 
     private void Update() {
         if (health < 5) {
-            long diff = (DateTime.Now.Ticks - _db.healthTime) / /* 60000000000 */ TIME_LEFT;
-            long RemainTime = (TIME_LEFT - (DateTime.Now.Ticks - _db.healthTime)) / 10000000;
+            long diff = (DateTime.Now.Ticks - _db.healthTime) / TIME_LEFT;
+            long RemainTime = (TIME_LEFT - (DateTime.Now.Ticks - _db.healthTime)) / 10000000 /* 1초 */;
+            RemainTime = Math.Max(RemainTime, 0);
 
-            print($"{(RemainTime / 60).ToString().PadLeft(2, '0')} : {(RemainTime % 60).ToString().PadLeft(2, '0')}");
+            if (!activeTimer) {
+                activeTimer = true;
+                _timeT.DOFade(.85f, 0.3f);
+            }
+
+            _timeT.text = $"{(RemainTime / 60).ToString().PadLeft(2, '0')}:{(RemainTime % 60).ToString().PadLeft(2, '0')}";
 
             if (diff >= 1) {
                 Give((int)diff);
                 _db.healthTime = DateTime.Now.Ticks;
                 print(health);
             }
+        } else if (activeTimer) {
+            activeTimer = false;
+            _timeT.DOFade(0, 0.3f);
         }
     }
 
