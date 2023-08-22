c5906d95494baf13e52db61bb5b46aec42ff35ce
dazb <jiwoo2426p@gmail.com>
2023-08-22 23:15:50 +0900

    .

--- Assets/Minki/0A_Scripts/BlockSystem/CreateBlock.cs
+++ Assets/Minki/0A_Scripts/BlockSystem/CreateBlock.cs
@@ -5,7 +5,7 @@ using DG.Tweening;
 
 public class CreateBlock : MonoBehaviour
 {
-    public static int stageInfo = 2;
+    public static int stageInfo = 9;
     public static int stageTileType = 1;
     public BatchSO batchSO;
 
